57b5412c7827dc882239b850a22255672ad4e368
Liviu Ionescu <ilg@livius.net>
2021-10-21 18:46:59 +0300

    tests/common-functions: install g++

--- scripts/tests/common-functions-source.sh
+++ scripts/tests/common-functions-source.sh
@@ -35,19 +35,20 @@ function update_image()
   local image_name="$1"
   
   # Make sure that the minimum prerequisites are met.
+  # The GCC libraries and headers are required by clang.
   if [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
   then
     run_verbose apt-get -qq update 
     run_verbose apt-get -qq install -y git-core curl tar gzip lsb-release binutils
-    run_verbose apt-get -qq install -y g++ libc6-dev libstdc++6 # TODO: get rid of them
+    run_verbose apt-get -qq install -y g++ libc6-dev libstdc++6
   elif [[ ${image_name} == *centos* ]] || [[ ${image_name} == *redhat* ]] || [[ ${image_name} == *fedora* ]]
   then
     run_verbose yum install -y -q git curl tar gzip redhat-lsb-core binutils
-    run_verbose yum install -y -q glibc-devel glibc-devel-static libstdc++-devel # TODO: get rid of them
+    run_verbose yum install -y -q glibc-devel glibc-devel-static libstdc++-devel 
   elif [[ ${image_name} == *suse* ]]
   then
     run_verbose zypper -q in -y git-core curl tar gzip lsb-release binutils findutils util-linux
-    run_verbose zypper -q in -y glibc-devel glibc-devel-static libstdc++6 # TODO: get rid of them
+    run_verbose zypper -q in -y glibc-devel glibc-devel-static libstdc++6 
   elif [[ ${image_name} == *manjaro* ]]
   then
     # run_verbose pacman-mirrors -g
@@ -56,7 +57,7 @@ function update_image()
     # Update even if up to date (-yy) & upgrade (-u).
     # pacman -S -yy -u -q --noconfirm 
     run_verbose pacman -S -q --noconfirm --noprogressbar git curl tar gzip lsb-release binutils
-    run_verbose pacman -S -q --noconfirm --noprogressbar gcc-libs # TODO: get rid of them
+    run_verbose pacman -S -q --noconfirm --noprogressbar gcc-libs
   elif [[ ${image_name} == *archlinux* ]]
   then
     run_verbose pacman -S -y -q --noconfirm 
