eb119664b1e7a54556cbb234e6decf88ed820e7a
Tom Feist-Hassan <tom.feist-hassan@camresp.com>
2022-09-09 17:20:44 +0100

    remove indentation and use proper EOF for heredoc

--- scripts/common-apps-functions-source.sh
+++ scripts/common-apps-functions-source.sh
@@ -1339,15 +1339,15 @@ function test_llvm()
 
     (
       run_app ${TEST_BIN_PATH}/clangd --check=hello-cpp.cpp
-      cat <<-EOTESTFILE > ${tmp}/unchecked-exception.cpp
-      // repro for clangd crash from github.com/clangd/clangd issue #1072
-        #include <exception>
-        int main() {
-            std::exception_ptr foo;
-            try {} catch (...) { }
-            return 0;
-        }
-EOTESTFILE
+      cat <<'__EOF__' > ${tmp}/unchecked-exception.cpp
+// repro for clangd crash from github.com/clangd/clangd issue #1072
+#include <exception>
+int main() {
+    std::exception_ptr foo;
+    try {} catch (...) { }
+    return 0;
+}
+__EOF__
       run_app ${TEST_BIN_PATH}/clangd --check=${tmp}/unchecked-exception.cpp
     )
 
