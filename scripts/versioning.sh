6cfa10649403af313b79d8bf0db073949b761b25
Liviu Ionescu <ilg@livius.net>
2022-11-20 18:08:37 +0200

    versioning.sh: rework build_common

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -13,7 +13,7 @@ function _xbb_activate_llvm_bootstrap_bins()
 {
   # Warning, this should not bring llvm-config into the PATH, since
   # it crashes the compiler-rt build.
-  export PATH="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin:${PATH}"
+  export PATH="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin:${PATH}"
 
   # Set LD_LIBRARY_PATH to XBB folders, refered by the bootstrap.
   xbb_activate_libs
@@ -23,53 +23,95 @@ function _prepare_bootstrap_cross_env()
 {
   unset_compiler_env
 
-  export CC="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-gcc"
-  export CXX="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-g++"
+  export CC="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-gcc"
+  export CXX="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-g++"
 
-  export AR="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-ar"
-  export AS="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-as"
-  export DLLTOOL="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-dlltool"
-  export LD="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-ld"
-  export NM="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-nm"
-  export OBJCOPY="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-objcopy"
-  export OBJDUMP="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-objdump"
-  export RANLIB="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-ranlib"
+  export AR="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-ar"
+  export AS="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-as"
+  export DLLTOOL="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-dlltool"
+  export LD="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-ld"
+  export NM="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-nm"
+  export OBJCOPY="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-objcopy"
+  export OBJDUMP="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-objdump"
+  export RANLIB="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-ranlib"
   # export READELF="${prefix}readelf"
   # export SIZE="${prefix}size"
-  export STRIP="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-strip"
-  export WINDRES="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-windres"
+  export STRIP="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-strip"
+  export WINDRES="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-windres"
   # export WINDMC="${prefix}windmc"
   # Use the XBB one, not the native llvm?
-  export RC="${XBB_BINARIES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_CROSS_COMPILE_PREFIX}-windres"
+  export RC="${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}${XBB_BOOTSTRAP_SUFFIX}/bin/${XBB_TARGET_TRIPLET}-windres"
 
   set_xbb_extras
 }
 
-function _build_mingw_bootstrap()
+function add_mingw_wrappers()
+{
+  (
+    # Add wrappers for the mingw-w64 binaries.
+    cd "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/bin"
+
+    cp "${XBB_BUILD_GIT_PATH}/wrappers"/*-wrapper.sh .
+
+    for exec in clang-target-wrapper dlltool-wrapper windres-wrapper llvm-wrapper
+    do
+      ${CC} "${XBB_BUILD_GIT_PATH}/wrappers/${exec}.c" -O2 -v -o ${exec}
+    done
+
+    for exec in clang clang++ gcc g++ cc c99 c11 c++ as
+    do
+      ln -sf clang-target-wrapper.sh ${XBB_TARGET_TRIPLET}-${exec}
+    done
+    for exec in addr2line ar ranlib nm objcopy strings strip
+    do
+      ln -sf llvm-${exec} ${XBB_TARGET_TRIPLET}-${exec}
+    done
+    if [ -f "llvm-windres" ]
+    then
+      # windres can't use llvm-wrapper, as that loses the original
+      # target arch prefix.
+      ln -sf llvm-windres ${XBB_TARGET_TRIPLET}-windres
+    else
+      ln -sf windres-wrapper ${XBB_TARGET_TRIPLET}-windres
+    fi
+    ln -sf dlltool-wrapper ${XBB_TARGET_TRIPLET}-dlltool
+    for exec in ld objdump
+    do
+      ln -sf ${exec}-wrapper.sh ${XBB_TARGET_TRIPLET}-${exec}
+    done
+  )
+}
+
+function build_mingw_clang_bootstrap()
 {
   # Build a bootstrap toolchain, that runs on Linux and creates Windows
   # binaries.
   (
-    # Make the use of XBB GCC explicit.
-    prepare_gcc_env "" "-xbb"
 
-    prepare_mingw_env "${XBB_MINGW_VERSION}" "${XBB_BOOTSTRAP_SUFFIX}"
+    # Build libraries refered by LLVM.
+    build_zlib "${XBB_ZLIB_VERSION}"
+    build_ncurses "${XBB_NCURSES_VERSION}"
+    build_libiconv "${XBB_LIBICONV_VERSION}"
+    build_xz "${XBB_XZ_VERSION}"
 
     # Deploy the headers, they are needed by the compiler.
     build_mingw_headers
 
     # Build LLVM with the host XBB compiler.
     # Has a reference to /opt/xbb/lib/libncurses.so.
-    build_llvm "${XBB_LLVM_VERSION}" "${XBB_BOOTSTRAP_SUFFIX}"
+    build_llvm "${XBB_LLVM_VERSION}" --bootstrap
+    add_mingw_wrappers
 
-    # Build gendef & widl with the host XBB compiler.
-    build_mingw_libmangle # Refered by gendef
-    build_mingw_gendef
+    # Build native widl & gendef.
     build_mingw_widl # Refers to mingw headers.
 
+    build_mingw_libmangle # Refered by gendef
+    build_mingw_gendef
+
     (
-      xbb_activate_llvm_bootstrap_bins
-      prepare_bootstrap_cross_env
+      # xbb_activate_llvm_bootstrap_bins
+      # prepare_bootstrap_cross_env
+      xbb_activate_installed_bin
 
       build_llvm_compiler_rt "${XBB_BOOTSTRAP_SUFFIX}"
 
@@ -82,77 +124,148 @@ function _build_mingw_bootstrap()
   )
 }
 
-function _build_common()
+function build_common()
 {
-  (
-    if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
+
+  if [ "${XBB_REQUESTED_HOST_PLATFORM}" == "win32" ]
+  then
+
+    # Build a bootstrap toolchain, mainly for the *-tblgen tools, but
+    # also because mixing with mingw-gcc fails the build in
+    # various weird ways.
+
+    # Number
+    XBB_MINGW_VERSION_MAJOR=$(echo ${XBB_MINGW_VERSION} | sed -e 's|\([0-9][0-9]*\)\..*|\1|')
+
+    XBB_MINGW_GCC_PATCH_FILE_NAME="gcc-${XBB_GCC_VERSION}-cross.patch.diff"
+
+    download_mingw "${XBB_MINGW_VERSION}"
+
+    # -------------------------------------------------------------------------
+    # Build the native dependencies.
+
+    xbb_set_target "mingw-w64-native"
+
+    build_mingw_clang_bootstrap
+
+    if false # Switch used during development to test bootstrap.
     then
+      xbb_activate_installed_bin
 
-      # Build a bootstrap toolchain, mainly for the *-tblgen tools, but
-      # also because mixing with mingw-gcc fails the build in
-      # various weird ways.
-      build_mingw_bootstrap
+      # -------------------------------------------------------------------------
+      # Build the target dependencies.
 
-      if true # Switch used during bootstrap tests.
-      then
-        # All of the following are cross compiled with the bootstrap LLVM
-        # and the results are Windows binaries.
-        xbb_activate_llvm_bootstrap_bins # Adjust paths.
-        prepare_bootstrap_cross_env # Define CC & family.
+      xbb_set_target
 
-        # Build libraries refered by LLVM.
-        build_zlib "${XBB_ZLIB_VERSION}"
-        build_ncurses "${XBB_NCURSES_VERSION}"
-        build_libiconv "${XBB_LIBICONV_VERSION}"
-        build_xz "${XBB_XZ_VERSION}"
-
-        # Build mingw-w64 components.
-        prepare_mingw_env "${XBB_MINGW_VERSION}"
-
-        build_mingw_headers
-        build_mingw_crt
-        build_mingw_winpthreads
-        build_mingw_winstorecompat
-        build_mingw_libmangle
-        build_mingw_gendef
-        build_mingw_widl
-
-        # Finally build LLVM clang.
-        build_llvm "${XBB_LLVM_VERSION}"
-
-        build_llvm_compiler_rt
-        build_llvm_libcxx # libunwind, libcxx, libcxxabi
-
-      fi
-
-    else # linux or darwin
-
-      # macOS has its own linker, cannot use the binutils ones.
-      if [ "${XBB_TARGET_PLATFORM}" == "linux" ]
-      then
-        # Build ld.gold to support LTO.
-        build_binutils_ld_gold "${XBB_BINUTILS_VERSION}"
-      fi
+      # All of the following are cross compiled with the bootstrap LLVM
+      # and the results are Windows binaries.
+      # xbb_activate_llvm_bootstrap_bins # Adjust paths.
+      prepare_bootstrap_cross_env # Define CC & family.
 
       # Build libraries refered by LLVM.
       build_zlib "${XBB_ZLIB_VERSION}"
-      build_libffi "${XBB_LIBFFI_VERSION}"
       build_ncurses "${XBB_NCURSES_VERSION}"
       build_libiconv "${XBB_LIBICONV_VERSION}"
       build_xz "${XBB_XZ_VERSION}"
-      build_libxml2 "${XBB_LIBXML2_VERSION}"
-      build_libedit "${XBB_LIBEDIT_VERSION}"
+
+      # -------------------------------------------------------------------------
+      # Build the application binaries.
+
+      xbb_set_executables_install_path "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
+      xbb_set_libraries_install_path "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
+
+      # Build mingw-w64 components.
+      build_mingw_headers
+      build_mingw_widl --program-prefix=
+      build_mingw_libmangle
+      build_mingw_gendef --program-prefix=
+
+      build_mingw_crt
+      build_mingw_winpthreads
+      build_mingw_winstorecompat
 
       # Finally build LLVM clang.
       build_llvm "${XBB_LLVM_VERSION}"
 
+      build_llvm_compiler_rt
+      build_llvm_libcxx # libunwind, libcxx, libcxxabi
+
     fi
-  )
+
+  else # linux or darwin
+
+    # -------------------------------------------------------------------------
+    # Build the native dependencies.
+
+    # autoreconf required by libxml2.
+
+    # https://ftp.gnu.org/pub/gnu/libiconv/
+    build_libiconv "${XBB_LIBICONV_VERSION}"
+
+    # https://ftp.gnu.org/gnu/autoconf/
+    # depends on m4.
+    build_autoconf "2.71"
+
+    # https://ftp.gnu.org/gnu/automake/
+    # depends on autoconf.
+    build_automake "1.16.5"
+
+    # http://ftpmirror.gnu.org/libtool/
+    build_libtool "2.4.7"
+
+    # configure.ac:34: error: Macro PKG_PROG_PKG_CONFIG is not available. It is usually defined in file pkg.m4 provided by package pkg-config.
+    # https://pkgconfig.freedesktop.org/releases/
+    # depends on libiconv
+    build_pkg_config "0.29.2"
+
+    # -------------------------------------------------------------------------
+    # Build the target dependencies.
+
+    xbb_set_target
+
+    if [ "${XBB_HOST_PLATFORM}" == "win32" ]
+    then
+      build_libiconv "${XBB_LIBICONV_VERSION}"
+    else
+      # Already built with the native dependencies.
+      :
+    fi
+
+    build_zlib "${XBB_ZLIB_VERSION}"
+    build_libffi "${XBB_LIBFFI_VERSION}"
+
+    XBB_NCURSES_DISABLE_WIDEC="y"
+    build_ncurses "${XBB_NCURSES_VERSION}"
+
+    build_xz "${XBB_XZ_VERSION}"
+    build_libxml2 "${XBB_LIBXML2_VERSION}"
+    build_libedit "${XBB_LIBEDIT_VERSION}"
+
+    # -------------------------------------------------------------------------
+    # Build the application binaries.
+
+    xbb_set_executables_install_path "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
+    xbb_set_libraries_install_path "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
+
+    # macOS has its own linker, cannot use the binutils ones.
+    if [ "${XBB_HOST_PLATFORM}" == "linux" ]
+    then
+      # Build ld.gold to support LTO.
+      build_binutils_ld_gold "${XBB_BINUTILS_VERSION}"
+    fi
+
+    # Finally build LLVM clang.
+    build_llvm "${XBB_LLVM_VERSION}"
+
+    # strip_libs
+
+  fi
+
 }
 
 function build_application_versioned_components()
 {
-  if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
+  if [ "${XBB_HOST_PLATFORM}" == "win32" ]
   then
     XBB_LLVM_BOOTSTRAP_BRANDING="${XBB_APPLICATION_DISTRO_NAME} bootstrap ${XBB_TARGET_MACHINE}"
 
@@ -164,6 +277,7 @@ function build_application_versioned_components()
   fi
 
   # https://github.com/llvm/llvm-project/releases/
+  # There are bug-fix releases every two weeks until X.0.5 or X.0.6 (if necessary).
   XBB_LLVM_VERSION="$(echo "${XBB_RELEASE_VERSION}" | sed -e 's|-.*||')"
   XBB_LLVM_PATCH_FILE_NAME="llvm-${XBB_LLVM_VERSION}.patch.diff"
 
@@ -198,72 +312,7 @@ function build_application_versioned_components()
     # https://www.thrysoee.dk/editline/
     XBB_LIBEDIT_VERSION="20210910-3.1" # "20210522-3.1"
 
-    # build_common
-
-    # -------------------------------------------------------------------------
-    # Build the native dependencies.
-
-    # autoreconf required by libxml2.
-
-    # https://ftp.gnu.org/pub/gnu/libiconv/
-    build_libiconv "${XBB_LIBICONV_VERSION}"
-
-    # https://ftp.gnu.org/gnu/autoconf/
-    # depends on m4.
-    build_autoconf "2.71"
-
-    # https://ftp.gnu.org/gnu/automake/
-    # depends on autoconf.
-    build_automake "1.16.5"
-
-    # http://ftpmirror.gnu.org/libtool/
-    build_libtool "2.4.7"
-
-    # configure.ac:34: error: Macro PKG_PROG_PKG_CONFIG is not available. It is usually defined in file pkg.m4 provided by package pkg-config.
-    # https://pkgconfig.freedesktop.org/releases/
-    # depends on libiconv
-    build_pkg_config "0.29.2"
-
-    # -------------------------------------------------------------------------
-    # Build the target dependencies.
-
-    xbb_set_target
-
-    if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
-    then
-      build_libiconv "${XBB_LIBICONV_VERSION}"
-    else
-      # Already built with the native dependencies.
-      :
-    fi
-
-    build_zlib "${XBB_ZLIB_VERSION}"
-    build_libffi "${XBB_LIBFFI_VERSION}"
-
-    XBB_NCURSES_DISABLE_WIDEC="y"
-    build_ncurses "${XBB_NCURSES_VERSION}"
-
-    build_xz "${XBB_XZ_VERSION}"
-    build_libxml2 "${XBB_LIBXML2_VERSION}"
-    build_libedit "${XBB_LIBEDIT_VERSION}"
-
-    # -------------------------------------------------------------------------
-    # Build the application binaries.
-
-    xbb_set_binaries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
-    xbb_set_binaries_install "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
-
-    # macOS has its own linker, cannot use the binutils ones.
-    if [ "${XBB_TARGET_PLATFORM}" == "linux" ]
-    then
-      # Build ld.gold to support LTO.
-      build_binutils_ld_gold "${XBB_BINUTILS_VERSION}"
-    fi
-
-    # Finally build LLVM clang.
-    build_llvm "${XBB_LLVM_VERSION}"
-
-    # strip_libs
+    build_common
 
     # -------------------------------------------------------------------------
   elif [[ "${XBB_RELEASE_VERSION}" =~ 13\.0\.1-[1] ]]
@@ -309,7 +358,7 @@ function build_application_versioned_components()
 
     # -------------------------------------------------------------------------
   else
-    echo "Unsupported ${XBB_APPLICATION_LOWER_CASE_NAME} version ${XBB_RELEASE_VERSION}."
+    echo "Unsupported ${XBB_APPLICATION_LOWER_CASE_NAME} version ${XBB_RELEASE_VERSION}"
     exit 1
   fi
 }
