e98eeb9dea5cc21f9a089f14c650a321f80486b2
Liviu Ionescu <ilg@livius.net>
2022-12-15 16:25:41 +0200

    tests/update.sh: instal g++ on docker

--- scripts/tests/update.sh
+++ scripts/tests/update.sh
@@ -13,31 +13,31 @@ function tests_update_system()
 {
   local image_name="$1"
 
+  export XBB_SKIP_32_BIT_TESTS="y"
+
   # Make sure that the minimum prerequisites are met.
-  # The GCC libraries and headers are required by clang.
   if [[ ${image_name} == github-actions-ubuntu* ]]
   then
-    : # sudo apt-get -qq install -y XXX
-  elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
+    :
+  elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]]
   then
-    run_verbose apt-get -qq install -y g++ libc6-dev libstdc++6
+    run_verbose apt-get -qq install --yes g++
+  elif [[ ${image_name} == *raspbian* ]]
+  then
+    run_verbose apt-get -qq install --yes g++
   elif [[ ${image_name} == *centos* ]] || [[ ${image_name} == *redhat* ]] || [[ ${image_name} == *fedora* ]]
   then
-    run_verbose yum install -y -q gcc-c++ glibc-devel libstdc++-devel
+    run_verbose yum install --assumeyes --quiet gcc-c++ glibc glibc-common
   elif [[ ${image_name} == *suse* ]]
   then
-    run_verbose zypper -q --no-gpg-checks in -y gcc-c++ glibc-devel libstdc++6 glibc-static
+    run_verbose zypper --quiet --no-gpg-checks install --no-confirm gcc-c++
   elif [[ ${image_name} == *manjaro* ]]
   then
-    run_verbose pacman -S -q --noconfirm --noprogressbar gcc gcc-libs
+    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc
   elif [[ ${image_name} == *archlinux* ]]
   then
-    run_verbose pacman -S -q --noconfirm --noprogressbar gcc gcc-libs
+    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc
   fi
-
-  echo
-  echo "The system C/C++ libraries..."
-  find /usr/lib* /lib -name 'libc.*' -o -name 'libstdc++.*' -o -name 'libgcc_s.*' -o -name 'libunwind*'
 }
 
 # -----------------------------------------------------------------------------
