438cc8f93e92bcf5fdf4f77304a47587e14be21f
Liviu Ionescu <ilg@livius.net>
2023-01-14 07:01:59 +0000

    Revert "llvm.sh: do not use ENABLE_RUNTIMES on linux"
    This reverts commit e6dfe3a7b177a6da4f34b5459ec730c549de2ce0.

--- scripts/dependencies/llvm.sh
+++ scripts/dependencies/llvm.sh
@@ -407,8 +407,7 @@ function llvm_build()
             config_options+=("-DLLVM_ENABLE_FFI=ON") # Arch
 
             config_options+=("-DLLVM_ENABLE_PROJECTS=clang;lld;lldb;clang-tools-extra;polly")
-            # Fails on Arm
-            # config_options+=("-DLLVM_ENABLE_RUNTIMES=compiler-rt;libcxx;libcxxabi;libunwind")
+            config_options+=("-DLLVM_ENABLE_RUNTIMES=compiler-rt;libcxx;libcxxabi;libunwind")
 
             config_options+=("-DLLVM_HOST_TRIPLE=${XBB_TARGET_TRIPLET}")
             config_options+=("-DLLVM_INSTALL_UTILS=ON")
