f744fe0640321905cda36ca2a734198f28d7a0da
Liviu Ionescu <ilg@livius.net>
2024-05-27 01:17:01 +0300

    llvm.sh: make c-cpp folder writable

--- scripts/dependencies/llvm.sh
+++ scripts/dependencies/llvm.sh
@@ -907,6 +907,7 @@ function llvm_test()
 
     source "${helper_folder_path}/tests/c-cpp/test-compiler.sh"
     run_verbose cp -Rv "${helper_folder_path}/tests/c-cpp" .
+    chmod -R a+w c-cpp
 
     run_verbose cp -Rv "${helper_folder_path}/tests/wine"/* c-cpp
     chmod -R a+w c-cpp
