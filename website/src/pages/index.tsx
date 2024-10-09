bcc4934870ca4e0b26163d8aa9045161fde9a936
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:32:37 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the LLVM clang executables">
+      description="A binary package with the LLVM clang executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
