9f56081d9a5f113283ab509a4edfa30259311898
Liviu Ionescu <ilg@livius.net>
2024-10-10 21:37:03 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -41,9 +41,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from xPack Binary Development Tools`}
-      description="A binary package with the LLVM clang executables">
+    <Layout 
+      title={`Hello from clang`}
+      description="A binary package with the clang executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
