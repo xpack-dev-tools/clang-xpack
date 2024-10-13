25d94e0edf3aa670f5f593a0f90a8e482c386800
Liviu Ionescu <ilg@livius.net>
2024-10-14 00:33:37 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Hello from clang`}
+      title={`Welcome to clang`}
       description="A binary package with the clang executables">
       <HomepageHeader />
       <main>
