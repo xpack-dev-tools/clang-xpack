9039e72e761fc6cef170f3369eeb26357b7ed281
Liviu Ionescu <ilg@livius.net>
2025-01-02 16:45:05 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -335,19 +335,29 @@ const config: Config = {
           'aria-label': 'GitHub repository',
         },
         {
-          label: `v${customFields.xpackVersion}`,
+          type: 'dropdown',
+          href: 'https://github.com/xpack-dev-tools/clang-xpack/',
+          position: 'right',
+          label: 'GitHub',
+          items: [
+            {
+              label: `clang-xpack`,
+              href: `https://github.com/xpack-dev-tools/clang-xpack/`,
+            },
+            {
+              label: 'xpack-dev-tools org',
+              href: 'https://github.com/xpack-dev-tools/',
+            },
+            {
+              label: 'xpack org',
+              href: 'https://github.com/xpack/',
+            },
+          ]
+        },
+        {
+          label: `${customFields.xpackVersion}`,
           position: 'right',
           href: `https://github.com/xpack-dev-tools/clang-xpack/releases/tag/v${customFields.xpackVersion}`,
-        },
-        {
-          href: 'https://github.com/xpack-dev-tools/',
-          label: 'xpack-dev-tools',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/xpack/',
-          label: 'xpack',
-          position: 'right',
         },
       ],
     },
@@ -408,11 +418,11 @@ const config: Config = {
               href: 'https://github.com/xpack-dev-tools/clang-xpack/',
             },
             {
-              label: 'GitHub xpack-dev-tools',
+              label: 'GitHub xpack-dev-tools org',
               href: 'https://github.com/xpack-dev-tools/',
             },
             {
-              label: 'GitHub xpack',
+              label: 'GitHub xpack org',
               href: 'https://github.com/xpack/',
             },
           ],
