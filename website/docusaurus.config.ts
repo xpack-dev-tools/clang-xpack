26347dbfbf52049c1dccbf4528a8cf76a9774ba4
Liviu Ionescu <ilg@livius.net>
2025-01-10 13:20:40 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,7 +89,8 @@ console.log('customFields: ' + util.inspect(customFields));
 // ----------------------------------------------------------------------------
 
 const config: Config = {
-  title: 'xPack LLVM clang',
+  title: 'xPack LLVM clang' +
+    ((process.env.DOCUSAURUS_IS_PREVIEW === 'true') ? ' (preview)' : ''),
   tagline: 'A binary distribution of LLVM clang',
   // Explicitly set in headTags.
   // favicon: '/img/favicon.ico',
@@ -260,7 +261,7 @@ const config: Config = {
       items: [
         {
           to: '/',
-          // label: 'Home',
+          label: 'clang',
           className: 'header-home-link',
           position: 'left'
         },
