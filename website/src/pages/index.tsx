959a3a9f6b625daf9caf3f4431751bd0e9501126
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:21:36 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/clang@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/clang@19.1.6-1.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
