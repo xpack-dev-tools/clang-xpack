64b1f6ebf1d732b765b2f2ec11f37f894ffd9437
Liviu Ionescu <ilg@livius.net>
2025-02-06 00:15:38 +0200

    llvm.sh: fix LLVM_BINUTILS_INCDIR

--- build-assets/scripts/dependencies/llvm.sh
+++ build-assets/scripts/dependencies/llvm.sh
@@ -459,9 +459,9 @@ function llvm_build()
             # binutils no longer used since 2.44.
             if [ "${XBB_BINUTILS_WITH_GOLD:-""}" == "y" ]
             then
-              config_options+=("-DLLVM_BINUTILS_INCDIR=${XBB_SOURCES_FOLDER_PATH}/binutils-${XBB_BINUTILS_VERSION}/include")
-            else
               config_options+=("-DLLVM_BINUTILS_INCDIR=${XBB_SOURCES_FOLDER_PATH}/binutils-with-gold-${XBB_BINUTILS_VERSION}/include")
+            else
+              config_options+=("-DLLVM_BINUTILS_INCDIR=${XBB_SOURCES_FOLDER_PATH}/binutils-${XBB_BINUTILS_VERSION}/include")
             fi
             config_options+=("-DLLVM_BUILD_LLVM_DYLIB=ON") # Arch
 
