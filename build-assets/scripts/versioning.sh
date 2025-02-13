c2b66a93e400071201ec63e7c650af5c4b811462
Liviu Ionescu <ilg@livius.net>
2025-02-13 18:26:19 +0200

    versioning.sh: ncurses --hack-link

--- build-assets/scripts/versioning.sh
+++ build-assets/scripts/versioning.sh
@@ -70,7 +70,7 @@ function clang_build_mingw_bootstrap()
   (
     # Build libraries refered by LLVM.
     zlib_build "${XBB_ZLIB_VERSION}"
-    ncurses_build "${XBB_NCURSES_VERSION}"
+    ncurses_build "${XBB_NCURSES_VERSION}" --hack-links
     libiconv_build "${XBB_LIBICONV_VERSION}"
     xz_build "${XBB_XZ_VERSION}"
 
@@ -193,7 +193,7 @@ function clang_build_common()
 
       # Build libraries refered by LLVM.
       zlib_build "${XBB_ZLIB_VERSION}"
-      ncurses_build "${XBB_NCURSES_VERSION}"
+      ncurses_build "${XBB_NCURSES_VERSION}" --hack-links
       libiconv_build "${XBB_LIBICONV_VERSION}"
       xz_build "${XBB_XZ_VERSION}"
 
@@ -274,7 +274,7 @@ function clang_build_common()
     zlib_build "${XBB_ZLIB_VERSION}"
     libffi_build "${XBB_LIBFFI_VERSION}"
 
-    ncurses_build "${XBB_NCURSES_VERSION}"
+    ncurses_build "${XBB_NCURSES_VERSION}" --hack-links
 
     xz_build "${XBB_XZ_VERSION}"
     libxml2_build "${XBB_LIBXML2_VERSION}"
